a9199282a3e39bddaa60419968bcc28a5662b98b
Joseph <josephartura@gmail.com>
2019-04-27 22:33:34 -0700

    Update PokerTester.java

--- Poker/src/PokerTester.java
+++ Poker/src/PokerTester.java
@@ -1,4 +1,7 @@
+import java.util.Arrays;
+
 public class PokerTester {
+    static final int numberOfPlayers = 5;
     public static void main(String[] args){
 
         String[] ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
@@ -9,8 +12,23 @@ public class PokerTester {
 
         CardMaker.printCard(CardMaker.constructCard());
         CardMaker.printCard(Deck.giveShuffle());
-        CardMaker.printCard(Deck.giveP1Cards());
-        CardMaker.printCard(Deck.giveP2Cards());
+        for(int i = 1; i <= numberOfPlayers; i++){
+            CardMaker.printCard(Deck.playerCards(i));
+        }
         CardMaker.printCard(Deck.giveTableCards());
+        System.out.println(Arrays.deepToString(Deck.checkPoint()));
+        for(int a = 0; a < Deck.checkPoint().length; a++){
+            for(int b = 0; b < Deck.checkPoint()[a].length; b++){
+                for(int c = 0; c < Deck.checkPoint()[a][b].length; c++){
+                    for(int d = 0; d < Deck.checkPoint()[a][b][c].length; d++){
+                        if(Deck.checkPoint()[a][b][c][d]){
+                            System.out.print(1);
+                        }else{
+                            System.out.print(0);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
