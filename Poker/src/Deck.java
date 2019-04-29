22ba48d915e9755f81c042156a3c297aeb91228e
Joseph <josephartura@gmail.com>
2019-04-28 22:03:45 -0700

    Update Deck.java

--- Poker/src/Deck.java
+++ Poker/src/Deck.java
@@ -1,22 +1,25 @@
 import java.util.ArrayList;
 
 public class Deck {
-    private static String[][] printCard;
+    private static String[][] printCards;
     private static ArrayList<Card> cards;
     private int deckSize;
 
-    Deck(String[] rank, String[] suit, int[] value){
+    Deck(String[] rank, String[] suit, int[] value, String[][] prints){
         cards = new ArrayList<>();
 
-        for(int j = 0; j < rank.length; j++){
+        int z = 0;
+        for(int j = 0; j < rank.length; j++) {
             for (String aSuit : suit) {
-                Card aCard = new Card(rank[j], aSuit, value[j]);
+                Card aCard = new Card(rank[j], aSuit, value[j], prints[z]);
+                z++;
                 cards.add(aCard);
             }
         }
         this.deckSize = cards.size();
-        printCard = CardMaker.constructCard();
         Shuffle();
+        printCards = passPrint();
+
     }
 
 //    boolean isEmpty(){
@@ -41,30 +44,38 @@ public class Deck {
             Card tempCard = cards.get(r);
             cards.set(r, cards.get(k));
             cards.set(k, tempCard);
-
-            String[] tempPrn = printCard[r];
-            printCard[r] = printCard[k];
-            printCard[k] = tempPrn;
         }
     }
 
-    static String[][] giveShuffle(){
-        return printCard;
+    static String[][] givePrint(){
+        return printCards;
+    }
+
+    private static String[][] passPrint(){
+        String[][] R = new String[52][5];
+        for(int i = 0; i < 52; i++)
+            R[i] = cards.get(i).Print();
+        return R;
     }
 
     private static Card[] cardsUsed(){
         Card[] cardData = new Card[((PokerTester.numberOfPlayers * 2) + 5)];
-        for(int i = 0; i < cardData.length; i++){
+        for(int i = 0; i < cardData.length; i++)
             cardData[i] = cards.get(i);
-        }
         return cardData;
     }
 
     static String[][] playerCards(int playerNum){
-        String[][] player = new String[2][PokerTester.numberOfPlayers];
-        for(int i = 0; i < player.length; i++){
-            player[i] = printCard[(2 * playerNum) - (i + 1)];
-        }
+        String[][][] player = new String[PokerTester.numberOfPlayers][2][5];
+        for(int i = player[0].length - 1; i >= 0; i--)
+            player[playerNum - 1][1 - i] = cards.get((2 * playerNum) - (i + 1)).Print();
+        return player[playerNum - 1];
+    }
+
+    static String[][] giveTableCards(){
+        String[][] player = new String[5][5];
+        for(int i = player[0].length - 1; i >= 0; i--)
+            player[i] = cards.get(i + (PokerTester.numberOfPlayers * 2)).Print();
         return player;
     }
 
@@ -79,22 +90,14 @@ public class Deck {
             }
         }
         if (table.length >= 0) System.arraycopy(cardsUsed(), (PokerTester.numberOfPlayers * 2), table, 0, table.length);
-        boolean[][][][] checkCards = new boolean[PokerTester.numberOfPlayers][table.length][2][2];
-        for(int i = 0; i < PokerTester.numberOfPlayers; i++){
-            for(int j = 0; j < table.length; j++){
-                for(int k = 0; k < 2; k++){
-                    checkCards[i][j][k][0] = Card.CompareSuit(players[i][k], table[j]);
-                    checkCards[i][j][k][1] = Card.CompareVal(players[i][k], table[j]);
-                }
-            }
-        }
-        return checkCards;
-    }
+        boolean[][][][] checkCards = new boolean[PokerTester.numberOfPlayers][2][2][table.length];
 
-    static String[][] giveTableCards(){
-        String[][] tableCards = new String[5][printCard[printCard.length - 1].length];
-        System.arraycopy(printCard, (PokerTester.numberOfPlayers * 2), tableCards, 0, tableCards.length);
-        return tableCards;
+        for(int i = 0; i < PokerTester.numberOfPlayers; i++)
+            for(int j = 0; j < table.length; j++)
+                for(int k = 0; k < 2; k++)
+                    for(int l = 0; l < 2; l++)
+                        checkCards[i][k][l][j] = Card.CompareSuit(players[i][k], table[j]);
+        return checkCards;
     }
 
     @Override
@@ -103,25 +106,20 @@ public class Deck {
 
         for (int i = deckSize - 1; i >= 0; i--) {
             rtn.append(cards.get(i));
-            if (i != 0) {
+            if (i != 0)
                 rtn.append(", ");
-            }
-            if ((deckSize - i) % 2 == 0) {
+            if ((deckSize - i) % 2 == 0)
                 rtn.append("\n");
-            }
         }
 
         rtn.append("\nDealt cards: \n");
         for (int i = cards.size() - 1; i >= deckSize; i--) {
-            if(i >= 0) {
+            if(i >= 0)
                 rtn.append(cards.get(i));
-            }
-            if (i != deckSize) {
+            if (i != deckSize)
                 rtn.append(", ");
-            }
-            if ((i - cards.size()) % 2 == 0) {
+            if ((i - cards.size()) % 2 == 0)
                 rtn.append("\n");
-            }
         }
 
         rtn.append("\n");
