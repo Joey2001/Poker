5adb8516d739b71fa1cb502aaabdd0ce6ec5754c
Joseph <josephartura@gmail.com>
2019-04-28 22:04:16 -0700

    Update PokerTester.java

--- Poker/src/PokerTester.java
+++ Poker/src/PokerTester.java
@@ -1,17 +1,17 @@
 import java.util.Arrays;
 
 public class PokerTester {
-    static final int numberOfPlayers = 5;
+    static final int numberOfPlayers = 2;
     public static void main(String[] args){
 
         String[] ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
         String[] suits = {"♥", "♣", "♦", "♠"};
         int[] pointValues = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 
-        Deck deck = new Deck(ranks, suits, pointValues);
+        Deck deck = new Deck(ranks, suits, pointValues, CardMaker.constructCard());
 
         CardMaker.printCard(CardMaker.constructCard());
-        CardMaker.printCard(Deck.giveShuffle());
+        CardMaker.printCard(Deck.givePrint());
         for(int i = 1; i <= numberOfPlayers; i++){
             CardMaker.printCard(Deck.playerCards(i));
         }
