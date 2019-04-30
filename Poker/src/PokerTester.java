232ea45c8702c5aabd811a921b08c9efd7fc5631
Joseph <josephartura@gmail.com>
2019-04-29 21:21:46 -0700

    Update PokerTester.java

--- Poker/src/PokerTester.java
+++ Poker/src/PokerTester.java
@@ -6,7 +6,7 @@ public class PokerTester {
 
         String[] ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
         String[] suits = {"♥", "♣", "♦", "♠"};
-        int[] pointValues = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+        int[] pointValues = {14, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 
         Deck deck = new Deck(ranks, suits, pointValues, CardMaker.constructCard());
 
@@ -16,12 +16,12 @@ public class PokerTester {
             CardMaker.printCard(Deck.playerCards(i));
         }
         CardMaker.printCard(Deck.giveTableCards());
-        System.out.println(Arrays.deepToString(Deck.checkPoint()));
-        for(int a = 0; a < Deck.checkPoint().length; a++){
-            for(int b = 0; b < Deck.checkPoint()[a].length; b++){
-                for(int c = 0; c < Deck.checkPoint()[a][b].length; c++){
-                    for(int d = 0; d < Deck.checkPoint()[a][b][c].length; d++){
-                        if(Deck.checkPoint()[a][b][c][d]){
+        System.out.println(Arrays.deepToString(Compare.checkPoint()));
+        for(int a = 0; a < Compare.checkPoint().length; a++){
+            for(int b = 0; b < Compare.checkPoint()[a].length; b++){
+                for(int c = 0; c < Compare.checkPoint()[a][b].length; c++){
+                    for(int d = 0; d < Compare.checkPoint()[a][b][c].length; d++){
+                        if(Compare.checkPoint()[a][b][c][d]){
                             System.out.print(1);
                         }else{
                             System.out.print(0);
