2d088eaedc91a590c4ea93cf19418a257105b8d8
Joseph <josephartura@gmail.com>
2019-04-30 21:37:31 -0700

    Update Compare.java

--- Poker/src/Compare.java
+++ Poker/src/Compare.java
@@ -1,6 +1,6 @@
 public class Compare {
     static Card[][] getData(){
-        Card[][] cards = new Card[PokerTester.numberOfPlayers][7];
+        Card[][] cards = new Card[Constants.numOfPlayers][7];
         for(int i = 0; i < cards.length; i++){
             cards[i] = Deck.passPlayerXTable(i);
         }
@@ -8,8 +8,8 @@ public class Compare {
     }
 
     static boolean[][][][] checkPoint(){
-        Card[][] players = new Card[PokerTester.numberOfPlayers][Deck.playerCards(1).length];
-        Card[] table = new Card[Deck.cardsUsed().length - (PokerTester.numberOfPlayers * 2)];
+        Card[][] players = new Card[Constants.numOfPlayers][Deck.playerCards(1).length];
+        Card[] table = new Card[Deck.cardsUsed().length - (Constants.numOfPlayers * 2)];
         int v = 0;
         for(int t = 0; t < players.length; t++){
             for(int u = 0; u < players[0].length; u++){
@@ -17,10 +17,10 @@ public class Compare {
                 v++;
             }
         }
-        if (table.length >= 0) System.arraycopy(Deck.cardsUsed(), (PokerTester.numberOfPlayers * 2), table, 0, table.length);
-        boolean[][][][] checkCards = new boolean[PokerTester.numberOfPlayers][2][2][table.length];
+        if (table.length >= 0) System.arraycopy(Deck.cardsUsed(), (Constants.numOfPlayers * 2), table, 0, table.length);
+        boolean[][][][] checkCards = new boolean[Constants.numOfPlayers][2][2][table.length];
 
-        for(int i = 0; i < PokerTester.numberOfPlayers; i++) {
+        for(int i = 0; i < Constants.numOfPlayers; i++) {
             for (int j = 0; j < table.length; j++) {
                 for (int k = 0; k < 2; k++) {
                     checkCards[i][k][0][j] = Card.CompareSuit(players[i][k], table[j]);
