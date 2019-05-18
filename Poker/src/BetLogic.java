2f785ba8ffcbedaa10717187d8d177e9f2a9ddf3
Joseph <josephartura@gmail.com>
2019-05-17 17:03:10 -0700

    Update BetLogic.java

--- Poker/src/BetLogic.java
+++ Poker/src/BetLogic.java
@@ -1,22 +1,35 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 class BetLogic {
-    static void Betting() throws IOException {
+    static double[] Betting(int subRound, boolean[][] folding) throws IOException {
         String output = "";
         double highestBid = -1;
         double pot = 0;
+        int playersIn = Constants.numOfPlayers;
 
         double[] playersBet = new double[Constants.numOfPlayers];
         boolean[] playerFold = new boolean[Constants.numOfPlayers];
-        for(int i = 0; i < playersBet.length; i++){
-            playersBet[i] = betting(i);
-            if(highestBid < playersBet[i]){
-                highestBid = playersBet[i];
+        for(int i = 0; i < folding.length; i++){
+            for(int j = 0; j < folding[0].length; j++){
+                if(folding[i][j]){
+                    playerFold[j] = true;
+                }
+            }
+        }
+        for (boolean aPlayerFold : playerFold) {
+            if (aPlayerFold) {
+                playersIn--;
+            }
+        }
+        for(int j = 0; j < playersBet.length; j++){
+            if (!playerFold[j]) {
+                playersBet[j] = betting(j);
+                if(playersBet[j] > highestBid)
+                    highestBid = playersBet[j];
             }
         }
         for (double aPlayersBet : playersBet) {
@@ -24,7 +37,7 @@ class BetLogic {
                 pot += aPlayersBet;
             }
         }
-        raiseBet(highestBid, playersBet, playerFold, pot, Constants.numOfPlayers);
+        raiseBet(highestBid, playersBet, playerFold, pot, playersIn);
 
         for(int i = 0; i < playersBet.length; i++){
             if(playersBet[i] >= 0){
@@ -37,8 +50,7 @@ class BetLogic {
         BufferedWriter out = new BufferedWriter(new FileWriter("c:/Users/Josep/OneDrive/Documents/outputTest1.txt"));
         out.write(output);
         out.close();
-
-        System.out.println(Arrays.toString(playersBet));
+        return playersBet;
     }
 
     private static void raiseBet(double highestBid, double[] playersBet, boolean[] playerFold, double pot, int playersIn){
@@ -58,8 +70,10 @@ class BetLogic {
             for (double aPlayersBet : playersBet)
                 if (aPlayersBet > 0)
                     pot += aPlayersBet;
-            if(playersIn != 0)
+            if(playersIn != 0) {
+                System.out.println(playersIn);
                 raiseBet(highestBid, playersBet, playerFold, pot, playersIn);
+            }
         }
     }
 
@@ -67,9 +81,9 @@ class BetLogic {
         Scanner betAmount = new Scanner(System.in);
         System.out.println("How much do you want to bet player " + (player + 1) + "?");
         String bets = betAmount.next();
-        if(!Pattern.matches("[a-zA-Z]+", bets)) {
+        if (!Pattern.matches("[a-zA-Z]+", bets) && Pattern.matches("[0-9]+", bets)) {
             return Math.abs(Double.parseDouble(bets));
-        }else{
+        } else {
             return -2;
         }
     }
