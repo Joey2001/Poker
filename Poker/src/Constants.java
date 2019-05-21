44da42f2d6b40fbb3ac78108e45d185351024d80
Joseph <josephartura@gmail.com>
2019-05-20 21:27:05 -0700

    Update Constants.java

--- Poker/src/Constants.java
+++ Poker/src/Constants.java
@@ -1,6 +1,7 @@
 class Constants {
-    static final int numOfPlayers = 2;
+    static int numOfPlayers;
     static final int timesToShuffle = 1;
     static final int numOfSubRounds = 3;
     static final double startingAmount = 500;
+    static final int spaces = 30;
 }
